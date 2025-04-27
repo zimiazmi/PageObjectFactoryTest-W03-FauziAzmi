4ecfed2ef4c63938c34d0c7b9bd821f8a0313f69
zimiazmi <zimiazmi04@gmail.com>
2025-04-27 07:12:54 +0700

    commit test

---
+++ src/main/java/com/juaracoding/PageObjectFactory/utils/DriverSingleton.java
@@ -0,0 +1,13 @@
+package com.juaracoding.PageObjectFactory.utils;
+
+/*
+IntelliJ IDEA 2024.3.3 (Community Edition)
+Build #IC-243.24978.46, built on February 11, 2025
+@Author zimiazmi a.k.a. Muhammad Fauzi Azmi
+Java Developer
+Created on 27/04/2025 07:12
+@Last Modified 27/04/2025 07:12
+Version 1.0
+*/
+public class DriverSingleton {
+}
